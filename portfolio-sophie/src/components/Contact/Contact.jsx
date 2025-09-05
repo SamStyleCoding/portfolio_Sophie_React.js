422f9024c31e8fdc348f91d1b3e4d94b3d751d9b
Sam Radinfar <hhesamradinfar@gmail.com>
2025-09-05 20:37:55 +0200

    react-toastify, Login, Modifier Contact done

--- portfolio-sophie/src/components/Contact/Contact.jsx
+++ portfolio-sophie/src/components/Contact/Contact.jsx
@@ -1,29 +1,133 @@
-import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Bounce } from 'react-toastify';
+
+
 
 export default function Contact() {
-	
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    try {
+      const response = await fetch('https://formsubmit.co/hhesamradinfar@gmail.com', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        toast.success("Votre message a été envoyé avec succès !", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: "colored",
+        });
+        e.target.reset();
+      } else {
+        toast.error("Une erreur s'est produite lors de lenvoi du message. Veuillez réessayer", {
+			position: "top-center",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: false,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "colored",
+			transition: Bounce,
+		});
+      }
+    } catch (error) {
+		toast.warn('Une erreur est survenue. Veuillez vérifier votre connexion internet.', {
+			position: "top-center",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: false,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "colored",
+			transition: Bounce,
+		});
+    }
+  };
+
+
   return (
-	<div>
-		<section className='contact'>
-			<h2 id='contacts'>Contact</h2>
-			<p>Vous avez un projet ? Discutons-en !</p>
-		</section>
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
-			<input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
-			<input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-			<input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
-			<select {...register("Title", { required: true })}>
-				<option value="Mr">Mr</option>
-				<option value="Mrs">Mrs</option>
-				<option value="Miss">Miss</option>
-			</select>
-			<input type="submit" />
-		</form>
-	</div>
+    <div>
+      <section className="contact">
+        <h2 id="contacts">Contactez-moi</h2>
+        <p>Vous avez un projet ? Je serais ravi d’en discuter avec vous !</p>
+      </section>
+
+      <form onSubmit={handleSubmit}>
+
+        <input type="hidden" name="_captcha" value="false" />
+        
+        <input
+          type="text"
+          name="FirstName"
+          placeholder="Prénom"
+          required
+          maxLength={80}
+        />
+        
+        <input
+          type="text"
+          name="LastName"
+          placeholder="Nom de famille"
+          required
+          maxLength={100}
+        />
+        
+        <input
+          type="email"
+          name="Email"
+          placeholder="Adresse e-mail"
+          required
+        />
+        
+        <input
+          type="tel"
+          name="MobileNumber"
+          placeholder="Numéro de téléphone"
+          required
+          minLength={6}
+          maxLength={14}
+          pattern="[0-9]{6,14}"
+          title="Veuillez entrer un numéro valide (6 à 14 chiffres)"
+        />
+        
+        <select name="Title" required>
+          <option value="Mr">Monsieur</option>
+          <option value="Mrs">Madame</option>
+          <option value="Miss">Mademoiselle</option>
+        </select>
+        
+        <input
+          type="text"
+          name="Message"
+          placeholder="Votre message"
+          required
+        />
+        
+        <input type="submit" value="Envoyer" />
+      </form>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
   );
 }
--- portfolio-sophie/src/pages/Login/Login.jsx
+++ portfolio-sophie/src/pages/Login/Login.jsx
@@ -1,9 +1,15 @@
-
-
 export default function Login() {
   return (
-	<div>
-	  <h1>Login</h1>
+	<div className="login">
+		<h1 className="log-in">Log in</h1>
+		<form className="form-login" action="#" method="post">
+			<label htmlFor="email">E-mail</label>
+			<input type="text" id="email" />
+			<label htmlFor="password">Mot de passe</label>
+			<input type="password" id="password" />
+			<button className="connecter" type="submit">Se connecter</button>
+			<p className="p-form">Mot de passe oublié</p>
+		</form>
 	</div>
   )
 }

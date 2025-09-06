c1c626be5062a66446b2742115a1517dc5b2ba63
Sam Radinfar <hhesamradinfar@gmail.com>
2025-09-06 12:25:36 +0200

    Créer le fichier api.rest et récupérer l'API des projets

--- portfolio-sophie/src/components/ButtonsProjet/ButtonsProjet.jsx
+++ portfolio-sophie/src/components/ButtonsProjet/ButtonsProjet.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 
 
 export default function ButtonsProjet() {
+
+	const [categories, setCategories] = useState([]);
+	const [error, setError] = useState(null);
+	const categoryClass = {
+		"Objets": "button-p",
+		"Appartements": "button-p-app",
+		"Hotels & restaurants": "button-p-hr"
+	};
+
+	useEffect(() => {
+		fetch("http://localhost:5678/api/categories")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Erreur réseau");
+			}
+			return response.json();
+		})
+		.then((data) => setCategories(data))
+		.catch((error) => setError(error.message));
+	}, []);
+
+	if (error) {
+		return <p>Une erreur est survenue: {error}</p>
+	}
+
   return (
 	<div className="buttons-projet">
 	  <button className="button-p">Tous</button>
-	  <button className="button-p">Objets</button>
-	  <button className="button-p-app">Appartements</button>
-	  <button className="button-p-hr">Hotels & restaurants</button>
+	  {
+	  	categories.map((cat) => (
+			<button key={cat.id} className={categoryClass[cat.name]}>{cat.name}</button>
+		))
+	  }
 	</div>
   )
 }
--- portfolio-sophie/src/components/ImagesProjet/ImagesProjet.jsx
+++ portfolio-sophie/src/components/ImagesProjet/ImagesProjet.jsx
@@ -1,53 +1,38 @@
+import { useEffect, useState } from "react";
 
 
 export default function ImagesProjet() {
+
+	const [works, setWorks] = useState([]);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch("http://localhost:5678/api/works")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Erreur réseau");
+			}
+			return response.json();
+		})
+		.then((data) => setWorks(data))
+		.catch((error) => setError(error.message))
+	}, []);
+
+	if (error) {
+		return <div>Erreur: {error}</div>;
+	}
+
   return (
 	<div>
 	  <div className="gallery">
-			<figure>
-				<img src="/abajour-tahina.png" alt="Abajour Tahina"/>
-				<figcaption>Abajour Tahina</figcaption>
-			</figure>
-			<figure>
-				<img src="/appartement-paris-v.png" alt="Appartement Paris V"/>
-				<figcaption>Appartement Paris V</figcaption>
-			</figure>
-			<figure>
-				<img src="/restaurant-sushisen-londres.png" alt="Restaurant Sushisen - Londres"/>
-				<figcaption>Restaurant Sushisen - Londres</figcaption>
-			</figure>
-			<figure>
-				<img src="/la-balisiere.png" alt="Villa “La Balisiere” - Port Louis"/>
-				<figcaption>Villa “La Balisiere” - Port Louis</figcaption>
-			</figure>
-			<figure>
-				<img src="/structures-thermopolis.png" alt="Structures Thermopolis"/>
-				<figcaption>Structures Thermopolis</figcaption>
-			</figure>
-			<figure>
-				<img src="/appartement-paris-x.png" alt="Appartement Paris X"/>
-				<figcaption>Appartement Paris X</figcaption>
-			</figure>
-			<figure>
-				<img src="/le-coteau-cassis.png" alt="Pavillon “Le coteau” - Cassis"/>
-				<figcaption>Pavillon “Le coteau” - Cassis</figcaption>
-			</figure>
-			<figure>
-				<img src="/villa-ferneze.png" alt="Villa Ferneze - Isola d’Elba"/>
-				<figcaption>Villa Ferneze - Isola d’Elba</figcaption>
-			</figure>
-			<figure>
-				<img src="/appartement-paris-xviii.png" alt="Appartement Paris XVIII"/>
-				<figcaption>Appartement Paris XVIII</figcaption>
-			</figure>
-			<figure>
-				<img src="/bar-lullaby-paris.png" alt="Bar “Lullaby” - Paris"/>
-				<figcaption>Bar “Lullaby” - Paris</figcaption>
-			</figure>
-			<figure>
-				<img src="/hotel-first-arte-new-delhi.png" alt="Hotel First Arte - New Delhi"/>
-				<figcaption>Hotel First Arte - New Delhi</figcaption>
-			</figure>
+		{
+			works.map((work) => (
+				<figure key={work.id}>
+					<img src={work.imageUrl} alt="Abajour Tahina"/>
+					<p>{work.title}</p>
+				</figure>
+			))
+		}
 		</div>
 	</div>
   )
